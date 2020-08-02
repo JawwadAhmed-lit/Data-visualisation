2005c885c3dd227e8de9fc4ce6fd58cc3e1e9fc4
JawwadAhmed-lit <jawwadahmed747@gmail.com>
2020-08-02 11:34:41 +0400

    colour changing and saving
    You can save the save the file too by using plt.savefig("filename.png", bbox_inches ="tight")

--- scatter_squares.py
+++ scatter_squares.py
@@ -1,16 +1,17 @@
 import matplotlib.pyplot as plt
 
-x_values = [1,2,3,4,5,6,7,8,9,10]
-y_values = [1,4,9,16,25,36,48,64,81,100]
+x_values = range(1,1001)
+y_values = [x**2 for x in x_values]
 
 
 plt.style.use('bmh')
 fig,ax = plt.subplots()
-ax.scatter(x_values,y_values, s=50)
+ax.scatter(x_values,y_values,c=y_values, cmap=plt.cm.Blues, s=10) # o to 1 only
 
 ax.set_title ("Plotting",fontsize = 24)
 ax.set_xlabel("numbers",fontsize = 20)
-ax.set_ylabel("answers",fontsize = 21)
+ax.set_ylabel("answers",fontsize = 11)
 ax.tick_params(axis = "both", which = "major", labelsize = 14)
-
-plt.show()
+ax.axis=([0,1100,0,110000])
+#plt.show()
+plt.savefig('squares_plot.png', bbox_inches='tight')
