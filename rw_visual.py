80177d3bb1598be5a96f0d28e36cce7154ab69c2
JawwadAhmed-lit <jawwadahmed747@gmail.com>
2020-08-04 11:51:09 +0400

    Experiments!

--- rw_visual.py
+++ rw_visual.py
@@ -2,14 +2,14 @@ import matplotlib.pyplot as plt
 from random_walk import RandomWalk
 while True:
 
-    rw = RandomWalk(50000) # better to keep at this rate!
+    rw = RandomWalk(5000) # better to keep at this rate!
     rw.fill_walk()
     plt.style.use('classic')
     fig , ax = plt.subplots(figsize=(12,9))
     point_running = range(rw.num_points)
-
     ax.scatter(rw.x_values,rw.y_values, c = point_running, cmap=plt.cm.Blues ,
-        edgecolors='none',s=1)
+        edgecolors='none',s=10)
+    #ax.plot(rw.x_values,rw.y_values,c= 'green' , linewidth = 3)
     ax.scatter(0, 0, c = 'green', edgecolors='none' , s =100)
     ax.scatter(rw.x_values[-1],rw.y_values[-1], c= 'red' , edgecolors='none', s = 100)
     # Remove the axes.
