0d29c52f3a9e7dff5eb46ec3f114bed5729cc031
JawwadAhmed-lit <jawwadahmed747@gmail.com>
2020-08-04 11:34:41 +0400

    Last commit!

--- rw_visual.py
+++ rw_visual.py
@@ -2,14 +2,19 @@ import matplotlib.pyplot as plt
 from random_walk import RandomWalk
 while True:
 
-    rw = RandomWalk()
+    rw = RandomWalk(50000) # better to keep at this rate!
     rw.fill_walk()
     plt.style.use('classic')
-    fig , ax = plt.subplots()
+    fig , ax = plt.subplots(figsize=(12,9))
     point_running = range(rw.num_points)
 
     ax.scatter(rw.x_values,rw.y_values, c = point_running, cmap=plt.cm.Blues ,
-        edgecolor='none',s=15)
+        edgecolors='none',s=1)
+    ax.scatter(0, 0, c = 'green', edgecolors='none' , s =100)
+    ax.scatter(rw.x_values[-1],rw.y_values[-1], c= 'red' , edgecolors='none', s = 100)
+    # Remove the axes.
+    ax.get_xaxis().set_visible(False)
+    ax.get_yaxis().set_visible(False)
     plt.show()
     keep_running = input("make another walk (y)/(n)")
     if keep_running == "n":
